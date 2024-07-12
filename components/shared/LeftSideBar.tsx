31f7b328c09589deccc7ac29261da51154082d6b
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-07-12 22:35:52 +0530

    Added Right Sidebar

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -79,7 +79,7 @@ const LeftSideBar = () => {
                   height={20}
                   className="invert-colors lg:hidden"
                 />
-                <span className="max-lg:hidden">Log In</span>
+                <span className="max-lg:hidden">Sign Up</span>
               </Button>
             </Link>
           </div>
