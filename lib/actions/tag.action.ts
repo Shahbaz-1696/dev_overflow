999d52819c0f53359ebf0ab21022dadd457e04bf
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-07-20 00:12:08 +0530

    Completed community page UI

---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { GetTopInteractedTagsParams } from "./shared.types";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+
+    // Get the interaction of user from questions, answers, comments, etc.
+    // Add interaction to database
+
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "1", name: "tag2" },
+      { _id: "3", name: "tag3" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
